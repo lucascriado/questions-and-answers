b37336c141790b6731e922d329f5cad370f14963
lucascriado <lucaasbrandini@gmail.com>
2023-04-26 22:23:50 -0300

    running .js exports

--- app.js
+++ app.js
@@ -7,3 +7,34 @@ console.log("Meu nome é Lucas!")
 if(!showSite){
     console.log(site)
 }
+
+function Sum(a, b){
+    return a+b
+}
+
+function Sub(a, b){
+    return a-b
+}
+
+function Multi(a, b){
+    return a*b
+}
+
+function Div(a, b){
+    return a/b
+}
+
+ResultadoDiv = Div(30, 10)
+ResultadoSub = Sub(30, 10)
+ResultadoSum = Sum(30, 10)
+ResultadoMulti = Multi(30, 10)
+
+console.log(ResultadoDiv, ResultadoMulti, ResultadoSub, ResultadoSum)
+
+module.exports = {
+    Multi,
+    Sum,
+    Div,
+    Sub
+};
+
---
+++ main.js
@@ -0,0 +1,12 @@
+const myFuctions = require('./app.js')
+
+resExp = myFuctions.Div(20, 20)
+resExp2 = myFuctions.Multi(20, 20)
+resExp3 = myFuctions.Sum(20, 20)
+resExp4 = myFuctions.Sub(20, 20)
+
+console.log(resExp)
+console.log(resExp2)
+console.log(resExp3)
+console.log(resExp4)
+
