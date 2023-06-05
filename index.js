f55a787a8664d3c16faffbaa1acd7809cbd6456d
lucascriado <lucaasbrandini@gmail.com>
2023-06-04 22:50:47 -0300

    organizing the files for npm and its nodemon

--- index.js
+++ index.js
@@ -5,4 +5,18 @@ app.get("/", function(req, res){
     res.send('olá MUNDOOO!')
 })
 
+app.get("/send/:randomvalue", function (req, res){
+    let server = req.params.randomvalue
+    console.log(server)
+})
+
+app.get("/send/", function(req, res){
+    let bundle = req.query['let'];
+    res.send('deu certo')
+    /* http://127.0.0.1:3333/send?let=x */
+    console.log(bundle)
+})
+
 app.listen(3333)
+
+
