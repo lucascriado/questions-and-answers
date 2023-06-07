9acdfc03a44270275da90cbf10cd54474e6d1d19
lucascriado <lucaasbrandini@gmail.com>
2023-06-06 22:26:29 -0300

    feat: organizing the files to start the project with ejs

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+app.get("/", function(req, res) {
+  res.render('index');
+});
+
+// Agora você pode referenciar a variável x aqui ou em qualquer outro lugar fora da função de callback
+
+app.listen(9090);
 
 
