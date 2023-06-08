0dffcec4ee05141991452453c297653d58059776
lucascriado <lucaasbrandini@gmail.com>
2023-06-08 16:08:28 -0300

    feat: learning how to use json stringify

--- index.js
+++ index.js
@@ -3,10 +3,25 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get("/:teste2", function(req, res) {
-  let server2 = req.params.teste2
-  res.render('index', {
-    lang: server2
+app.get("/", function(req, res) {
+
+  var produtos = [
+    {nome: "Teste11", valor: 5.55},
+    {nome1: "Teste22", valor: 7.35},
+    {nome2: "Teste33", valor: 7.35}
+  ]
+
+  console.log(produtos)
+
+  let produtosLista = JSON.stringify(produtos)
+
+  let lang = 'teste123'
+
+  res.render("index", {
+    produtosLista: produtosLista,
+    produtos: produtosLista,
+    lang: lang,
+    produtos: produtos
   });
 });
 
