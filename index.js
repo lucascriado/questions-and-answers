75e5e9b44a1a438522cd4ee47c87ca8227968a92
lucascriado <lucaasbrandini@gmail.com>
2023-06-08 11:49:38 -0300

    feat: learning how to use ejs with html integrated with js

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get("/:teste1/:teste2", function(req, res) {
-  let server1 = req.params.teste1
+app.get("/:teste2", function(req, res) {
   let server2 = req.params.teste2
   res.render('index', {
-    name: server1,
     lang: server2
   });
 });
-
 
 app.listen(9090);
 
