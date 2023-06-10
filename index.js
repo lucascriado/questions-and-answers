805eab9a9b33e493d33d2afc25a663d108f92a8c
lucascriado <lucaasbrandini@gmail.com>
2023-06-10 14:05:44 -0300

    feat: organizing partials

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ app.set('view engine', 'ejs');
 /* usando os arquivos estaticos */
 app.use(express.static('public'));
 
-/* abrindo uma rota */
-app.get("/", function(req, res) {
-  /* renderizando uma pagina */
-  res.render("index");
-});
-
 app.get("/perguntar", function(req, res){
   res.render("question")
 })
