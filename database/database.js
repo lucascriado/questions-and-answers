bd659ce7a24c25a02b1ade849b03e01d14efec58
lucascriado <lucaasbrandini@gmail.com>
2023-06-11 17:49:53 -0300

    feat: organizing db

--- database/database.js
+++ database/database.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const connect = new Sequelize('forms', 'root', '1337',{
     host: 'localhost',
     dialect: 'mysql', 
-    port: 3306
+    port: 3307
 })
 
 module.exports = connect;
