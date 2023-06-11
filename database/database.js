931fee6062ae6fc4370718b867348659baf3d55e
lucascriado <lucaasbrandini@gmail.com>
2023-06-11 12:09:49 -0300

    feat: organizing db

---
+++ database/database.js
@@ -0,0 +1,6 @@
+const Sequelize = require('sequelize');
+
+const connect = new Sequelize('forms', 'root', '1337',{
+    host: 'localhost',
+    dialect: 'mysql'
+})
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* modulos/bibliotecas instalados */
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 
 /* como usar o ejs */
 app.set('view engine', 'ejs');
@@ -8,9 +9,25 @@ app.set('view engine', 'ejs');
 /* usando os arquivos estaticos */
 app.use(express.static('public'));
 
-app.get("/perguntar", function(req, res){
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get("/perguntar", function (req, res) {
   res.render("question")
 })
 
+app.post("/salvar", function (req, res) {
+  res.send("formulário recebido!")
+  var reqNome = req.body.nome;
+  var reqEmail = req.body.email;
+  console.log(reqEmail + reqNome)
+})
+
 app.listen(9090);
+
+
+
+
+
+
 
