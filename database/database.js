640a4caa702b848da0a1b9a06a38fc85a3f2033b
lucascriado <lucaasbrandini@gmail.com>
2024-01-27 18:19:33 -0300

    fix: struct data

--- database/database.js
+++ database/database.js
@@ -1,17 +1,8 @@
 const Sequelize = require('sequelize');
 
-const connect = new Sequelize('questions', 'root', '1337',{
+const connect = new Sequelize('questions', 'admin', '1337',{
     host: 'localhost',
     dialect: 'mysql', 
 })
 
 module.exports = connect;
-
-/* const Sequelize = require('sequelize');
-
-const connect = new Sequelize('forms', 'lucascriado', '1337',{
-    host: 'localhost',
-    dialect: 'mysql',
-})
-
-module.exports = connect; */
--- index.js
+++ index.js
@@ -16,22 +16,18 @@ connect
 
 /* como usar o ejs */
 app.set('view engine', 'ejs');
-
-/* usando os arquivos estaticos */
-app.use(express.static('public'));
-
-app.use(bodyparser.urlencoded({ extended: false }))
-app.use(bodyparser.json())
+app.use(express.static('public'), bodyparser.urlencoded({ extended: false }), bodyparser.json());
 
 app.post("/save", function (req, res) {
   var reqname = req.body.name;
   var reqtext = req.body.text;
+  console.log(reqname, reqtext)
   if (reqname && reqtext) {
     answer.create({
       name: reqname,
-      email: reqtext
+      question: reqtext
     })
-    res.redirect("/");
+    res.redirect("/community");
   } else {
     console.log('alguma coisa deu errado')
   }
@@ -39,12 +35,14 @@ app.post("/save", function (req, res) {
 
 app.get("/community", function (req, res) {
   answer.findAll({ raw: true }).then(prm => {
-    res.render("community")
+    res.render("community", { data: prm })
   })
 })
 
-app.get("/register", function (req, res) {
-  res.render("register")
+app.get("/community/:id", function (req, res) {
+  answer.findOne({ where: { id: req.params.id } }).then(prm => {
+    res.render("question", { data: prm })
+  })
 })
 
 app.listen(9090);
