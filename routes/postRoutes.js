325809fe510fba58b1d17c9ece88493e6ecae3ad
lucascriado <lucaascriado@gmail.com>
2024-03-12 07:01:34 -0300

    update post message

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -51,14 +51,14 @@ router.post('/editPost/:id', authenticateJWT, async (req, res) => {
     const id = req.params.id;
     const userId = req.user.userId;
     const avatar = req.user.avatar;
-    const { content } = req.body;
+    const { title, content } = req.body;
     
     try {
         const post = await Post.findOne({ where: { id: id, userId: userId } });
         if (!post) {
             return res.status(404).json({ message: 'Post not found' });
         }
-        await post.update({ content });
+        await post.update({ title, content });
         res.redirect('/welcome');
     } catch (error) {
         res.status(500).json({ message: 'Error updating post', error });
